86c3c08c1838fdd00d07297821855ced87840329
mvujovich <maqv10@student.cis.edu.hk>
2021-04-14 23:32:01 +0800

    Add XCode files (empty)

---
+++ Arts_Explorer/Arts_ExplorerTests/Arts_ExplorerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Arts_ExplorerTests.swift
+//  Arts_ExplorerTests
+//
+//  Created by Mirjana Aleksandra Qi Vujovich on 14/4/2021.
+//
+
+import XCTest
+@testable import Arts_Explorer
+
+class Arts_ExplorerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
