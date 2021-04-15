912e917577663ff217e6b1ffb906819b95081f8a
mvujovich <maqv10@student.cis.edu.hk>
2021-04-15 20:40:33 +0800

    Added more ViewControllers

--- Arts_Explorer/Arts_Explorer/AppDelegate.swift
+++ Arts_Explorer/Arts_Explorer/AppDelegate.swift
@@ -6,17 +6,20 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UITabBar.appearance().barTintColor = .black
         UITabBar.appearance().tintColor = .white
+        FirebaseApp.configure()
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
---
+++ Arts_Explorer/Arts_Explorer/View_Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Arts_Explorer
+//
+//  Created by Mirjana Aleksandra Qi Vujovich on 14/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // get screen size object.
+    }
+}
