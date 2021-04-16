2498ed631d19ddf764a9f77d81ae956db54759f6
mvujovich <maqv10@student.cis.edu.hk>
2021-04-16 22:22:38 +0800

    Posts can be uploaded and saved to Firebase (...with UI bugs)

--- Arts_Explorer/Arts_Explorer/View_Controllers/UploadViewController.swift
+++ Arts_Explorer/Arts_Explorer/View_Controllers/UploadViewController.swift
@@ -6,13 +6,105 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var imageHolder: UIImageView!
+    
+    @IBOutlet weak var categorySelecter: UISegmentedControl!
+    
+    @IBOutlet weak var uploadButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpElements()
         // Do any additional setup after loading the view.
+    }
+    
+    func setUpElements() {
+        errorLabel.alpha = 0
+        uploadButton.layer.cornerRadius = 5
+        //Add more later
+    }
+    
+    @IBAction func uploadPressed(_ sender: Any) {
+        
+        //Validate fields
+        let error = validateFields()
+        
+        if error != nil {
+            //If there is an error:
+            showError(message: error!) //Exclamation point to force unwrap
+        }
+        else {
+            
+            //All fields have been validated; now force-unwrapped
+            let titleText: String = titleTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let descriptionText: String = descriptionTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let selectedItemInt: Int = categorySelecter.selectedSegmentIndex
+            let postUUID = UUID().uuidString
+            
+            var selectedCategory: String = "music"
+            
+            if selectedItemInt == 0 {
+                selectedCategory = "art"
+            }
+            else if selectedItemInt == 1 {
+                selectedCategory = "drama"
+            }
+            else if selectedItemInt == 2 {
+                selectedCategory = "film"
+            }
+            
+            let firestore = Firestore.firestore()
+            let commentArray = [String]()
+            //Do an array for categories later... fix it!
+        
+            firestore.collection("posts").addDocument(data:
+                        [   //Data saved in Dictionary
+                            "id": postUUID,
+                            "op": "figure_out_later",
+                            "approved": false,
+                            "comments": commentArray,
+                            "category": selectedCategory,
+                            "mediaID": "figure_out_later",
+                            "title": titleText,
+                            "description": descriptionText
+                        ]) { (error) in
+                            if error != nil { //User object not saved to Firebase
+                                self.showError(message: "Post could not be added.")
+                            }
+                        }
+                    }
+    }
+    
+    func validateFields() -> String? { //Method returns optional String
+        
+        let titleText: String = titleTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        let descriptionText: String = descriptionTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        
+        //Check all fields are filled in
+        if titleText == "" && descriptionText == "" //Fix this to be no media OR title OR desc
+        {
+            return "Please fill in all fields."
+        }
+        
+        //Category defaults to art; no need to check
+        
+        return nil
+    }
+
+    func showError(message: String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1 //Make error text visible
     }
     
 
